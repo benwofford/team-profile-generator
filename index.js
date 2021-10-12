9290a031e9abe0b0d08e635db94b4081a89b877d
BenWofford <woffordbt@gmail.com>
2021-10-12 18:28:18 -0400

    built out generatehtml.js with tutor

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const inquirer = require('inquirer');
-const crypto = require('crypto');
 const Employee = require('./lib/employee');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
+const Genhtml= require('./lib/generatehtml');
 
-// enter team managers name
-// enter employee ID, email and office number
-// loop menu if adding additional team members
 
+let managerData;
 const mainPrompts = [
     {
         type: 'list',
         name: 'menuChoice',
-        choices: ['Add new employee', 'Write to html', 'Exit']
+        choices: ['Add new employee', 'Write to HTML', 'Exit']
     }
 ]
 const empTypes = [
@@ -41,54 +39,50 @@ const empInfo = [
     }
 ]
 async function genEmployee (prompts) {
-    // TODO: generate engineer or intern prompt and repeat if necessary
     let prompt = inquirer.createPromptModule();
     let addEmpMenu = await prompt(prompts);
     return addEmpMenu
 };
 
 async function userInput (promptData,engArray=[],internArray=[]) {
-    // TODO: create prompts specific to each class as a class method if possible
     console.log("-----------------------");
     console.log("Welcome to team creator");
     console.log("-----------------------");
-    //let engineerAdded = [];
-    //let internAdded = [];
+
 
     let prompt = inquirer.createPromptModule();
     let mainMenu = await prompt(promptData);
     if (mainMenu.menuChoice === "Add new employee") {
-        // TODO: generate manager first
-        // Instantiate classes
-        let manager = new Manager();
-        let engineer = new Engineer();
+        // let engineer = new Engineer();
         let intern = new Intern();
         console.log("Enter manager's information---")
-        let managerData = await genEmployee(empInfo);
+        let managerAnswers = await genEmployee(empInfo);
+        managerData = new Manager(managerAnswers.name,managerAnswers.id,managerAnswers.email,managerAnswers.officeNumber);
         console.log("Add any employees that report to manager---")
         let addEmp = true;
         while (addEmp) {
             let empMenu = await genEmployee(empTypes);
-            // TODO: build an external source to push to instead of pushing arrays?
             if (empMenu.menuChoice === "Add Engineer"){
                 console.log("Enter engineer's information---");
-                let engData = await engineer.engData();
-                engData = {role: engineer.getRole(), data: engData};
-                // push this to engineerAdded array to write to HTML later
-                engArray.push(engData);
+                let engData = await Engineer.engData();
+                const eng = new Engineer(engData.name, engData.id,engData.email,engData.github);
+                engArray.push(eng);
             }
             else if (empMenu.menuChoice === "Add Intern"){
                 console.log("Enter intern's information---");
                 let internData = await intern.internData();
-                internData = {role: intern.getRole(), data: internData};
-                internArray.push(internData);
+                //internData = {role: intern.getRole(), data: internData};
+                internArray.push(intern);
             }
             else if (empMenu.menuChoice){
-                return userInput(mainPrompts, engArray,internArray);
+                return userInput(mainPrompts,engArray,internArray);
             }
         }
-    }
-    else if (mainMenu.menuChoice === "Exit") {
+    } else if (mainMenu.menuChoice === "Write to HTML") {
+        const generate = new Genhtml('team.html', {managerData,engArray,internArray})
+        // console.log(generate.writeHtml());
+    } else if (mainMenu.menuChoice === "Exit") {
+        console.log(managerData);
         console.log(engArray);
         console.log(internArray);
         console.log("exiting...")
@@ -97,14 +91,6 @@ async function userInput (promptData,engArray=[],internArray=[]) {
     // TODO: return objects to pass to Genhtml
 }
 
-let myEmployee = new Employee('Hank', 420,'hankhill@stricklandpropane.com');
-randID = crypto.randomUUID();
-let genUser = {
-    //[randId]: myEmployee TODO: generate UUID
-}
-for (const x in genUser) {
-    console.log(genUser[x].name);
-}
+let myEmployee = new Employee('Hank', 7,'hankhill@stricklandpropane.com');
 
-// TODO: write out HTML to file
 userInput(mainPrompts);
