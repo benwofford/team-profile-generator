14d28cf502d7d698979e436bb0f6422ada3f569c
BenWofford <woffordbt@gmail.com>
2021-10-15 14:50:35 -0400

    cleaned up index.js

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const Intern = require('./lib/intern');
 const Genhtml= require('./lib/generatehtml');
 
 
-let managerData;
 const mainPrompts = [
     {
         type: 'list',
         name: 'menuChoice',
-        choices: ['Add new employee', 'Write to HTML', 'Exit']
+        choices: ['Add new team', 'Write to HTML', 'Exit']
     }
 ]
 const empTypes = [
@@ -21,30 +20,13 @@ const empTypes = [
         choices: ['Add Engineer', 'Add Intern', 'Finished adding employees']
     }
 ]
-const empInfo = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'Employee name: '
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'Employee ID: '
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Employee email: '
-    }
-]
 async function genEmployee (prompts) {
     let prompt = inquirer.createPromptModule();
     let addEmpMenu = await prompt(prompts);
     return addEmpMenu
 };
 
-async function userInput (promptData,engArray=[],internArray=[]) {
+async function userInput (promptData,empArray=[]) {
     console.log("-----------------------");
     console.log("Welcome to team creator");
     console.log("-----------------------");
@@ -52,45 +34,44 @@ async function userInput (promptData,engArray=[],internArray=[]) {
 
     let prompt = inquirer.createPromptModule();
     let mainMenu = await prompt(promptData);
-    if (mainMenu.menuChoice === "Add new employee") {
-        // let engineer = new Engineer();
+    if (mainMenu.menuChoice === "Add new team") {
+        let manager = new Manager();
+        let engineer = new Engineer();
         let intern = new Intern();
+
         console.log("Enter manager's information---")
-        let managerAnswers = await genEmployee(empInfo);
-        managerData = new Manager(managerAnswers.name,managerAnswers.id,managerAnswers.email,managerAnswers.officeNumber);
-        console.log("Add any employees that report to manager---")
+        let managerAnswers = await manager.managerAnswers;
+        managerAnswers.role = manager.getRole();
+        empArray.push(managerAnswers)
+
+        console.log("Add employees that report to manager---")
         let addEmp = true;
         while (addEmp) {
             let empMenu = await genEmployee(empTypes);
             if (empMenu.menuChoice === "Add Engineer"){
                 console.log("Enter engineer's information---");
-                let engData = await Engineer.engData();
-                const eng = new Engineer(engData.name, engData.id,engData.email,engData.github);
-                engArray.push(eng);
+                let engData = await engineer.engData();
+                engData.role = engineer.getRole();
+                engArray.push(engData);
             }
             else if (empMenu.menuChoice === "Add Intern"){
                 console.log("Enter intern's information---");
                 let internData = await intern.internData();
-                //internData = {role: intern.getRole(), data: internData};
-                internArray.push(intern);
+                internData.role = intern.getRole();
+                empArray.push(internData);
             }
             else if (empMenu.menuChoice){
-                return userInput(mainPrompts,engArray,internArray);
+                return userInput(mainPrompts,empArray);
             }
         }
     } else if (mainMenu.menuChoice === "Write to HTML") {
-        const generate = new Genhtml('team.html', {managerData,engArray,internArray})
-        // console.log(generate.writeHtml());
+        let genHtml = new Genhtml('./dist/index.html', empArray);
+        genHtml.writeHtml();
     } else if (mainMenu.menuChoice === "Exit") {
-        console.log(managerData);
-        console.log(engArray);
-        console.log(internArray);
+        console.log(empArray);
         console.log("exiting...")
         return
     }
-    // TODO: return objects to pass to Genhtml
 }
-
-let myEmployee = new Employee('Hank', 7,'hankhill@stricklandpropane.com');
 
 userInput(mainPrompts);
